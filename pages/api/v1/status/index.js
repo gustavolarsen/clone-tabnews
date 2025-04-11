e5b3567a3dc07395fa5710e002ae3ed9ada6f4d0
Gustavo Larsen <guslarsen@gmail.com>
2025-04-10 21:06:25 -0300

    adiciona arquivos `databes.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-const status = (request, response) => {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("SELECT 1;");
   response.status(200).json({
     status: "ok",
     message: "Server is running smoothly",
